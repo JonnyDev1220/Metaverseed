0a93a8ac190fdf82b8bab76e72d98a064211ec26
Jonny3 <85959883+JonnyDev1220@users.noreply.github.com>
2023-04-27 21:54:08 +0200

    Update ClientApp.js

--- firebase/clientApp.js
+++ firebase/clientApp.js
@@ -4,15 +4,8 @@ import { getFirestore } from "@firebase/firestore";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA7oUMyANsrmDzWfVAjMWVRv7i1mBVBa_w",
-  authDomain: "metabazar-c4a47.firebaseapp.com",
-  databaseURL:
-    "https://metabazar-c4a47-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "metabazar-c4a47",
-  storageBucket: "metabazar-c4a47.appspot.com",
-  messagingSenderId: "393533803822",
-  appId: "1:393533803822:web:bb0def5349ff585e777ebf",
-  measurementId: "G-8FJFVQF4ZB",
+
+  //you firebase config
 };
 
 // Initialize Firebase
